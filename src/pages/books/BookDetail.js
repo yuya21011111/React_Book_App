5140977438d8ba6e462edd293629635e8cf2ba3c
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-10 16:28:57 +0900

    BookEditデザイン

--- src/pages/books/BookDetail.js
+++ src/pages/books/BookDetail.js
@@ -1,6 +1,55 @@
-const BookDetail = () => {
+import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, TextField, Typography } from "@mui/material"
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+const BookDetail = ({ books }) => {
+    const params = useParams()
+    const book = books.find(book => {
+        return book.id === parseInt(params.id, 10)
+    })
+
     return(<>
-    BookDetail
+      <Container component="section" maxWidth="md" 
+      sx={{mt:5}}>
+        <Card sx={{ height: '100%' }}>
+            <Grid container>
+                <Grid item sm={4}>
+                    <CardMedia
+                      component="img"
+                      image={book.image}
+                      alt={book.title} />
+                </Grid>
+                <Grid item sm={8}>
+                    <CardContent>
+                        <Typography sx={{ md:2, fontSize: '18px' }}>
+                            {book.title}
+                        </Typography>
+                        <Box sx={{md:2}}>
+                            読んだ日
+                        </Box>
+                        <Box>
+                            感想：<br/>
+                            <TextField
+                            multiline
+                            fullWidth
+                            rows={8} />
+                        </Box>
+                    </CardContent>
+                    <CardActions>
+                        <Grid container>
+                            <Grid item sm={6}>
+                                <Button component={Link} to='/'
+                                color="secondary" variant="contained">一覧に戻る</Button>
+                            </Grid>
+                            <Grid item sm={6}>
+                                <Button color="info" variant="contained">保存する</Button>
+                            </Grid>
+                        </Grid>
+                    </CardActions>
+                </Grid>
+            </Grid>
+        </Card>
+
+      </Container>
     </>)
 }
 
