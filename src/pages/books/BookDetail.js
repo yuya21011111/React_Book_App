5f1738680beff783e3c19826ff944db001e05bf8
YuyaYamazi <kaimomo12345@gmail.com>
2023-09-11 16:08:49 +0900

    DatePicker導入

--- src/pages/books/BookDetail.js
+++ src/pages/books/BookDetail.js
@@ -1,55 +1,72 @@
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, TextField, Typography } from "@mui/material"
 import { useParams } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from "@mui/x-date-pickers"
+import ja from "date-fns/locale/ja"
+import { useState } from "react"
+
 const BookDetail = ({ books }) => {
+    const [value, setValue] = useState(null)
     const params = useParams()
     const book = books.find(book => {
         return book.id === parseInt(params.id, 10)
     })
 
-    return(<>
-      <Container component="section" maxWidth="md" 
-      sx={{mt:5}}>
-        <Card sx={{ height: '100%' }}>
-            <Grid container>
-                <Grid item sm={4}>
-                    <CardMedia
-                      component="img"
-                      image={book.image}
-                      alt={book.title} />
-                </Grid>
-                <Grid item sm={8}>
-                    <CardContent>
-                        <Typography sx={{ md:2, fontSize: '18px' }}>
-                            {book.title}
-                        </Typography>
-                        <Box sx={{md:2}}>
-                            読んだ日
-                        </Box>
-                        <Box>
-                            感想：<br/>
-                            <TextField
-                            multiline
-                            fullWidth
-                            rows={8} />
-                        </Box>
-                    </CardContent>
-                    <CardActions>
-                        <Grid container>
-                            <Grid item sm={6}>
-                                <Button component={Link} to='/'
-                                color="secondary" variant="contained">一覧に戻る</Button>
+    return (<>
+        <Container component="section" maxWidth="md"
+            sx={{ mt: 5 }}>
+            <Card sx={{ height: '100%' }}>
+                <Grid container>
+                    <Grid item sm={4}>
+                        <CardMedia
+                            component="img"
+                            image={book.image}
+                            alt={book.title} />
+                    </Grid>
+                    <Grid item sm={8}>
+                        <CardContent>
+                            <Typography sx={{ md: 2, fontSize: '18px' }}>
+                                {book.title}
+                            </Typography>
+                            <Box sx={{ md: 2 }}>
+                                読んだ日:
+                                <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ja}
+                                dateFormats={{ monthAndYear: "yyyy年 MM月" }}>
+                                    <DatePicker label="日付" value={value}
+                                    maxDate={new Date()}
+                                    onChange={(newValue) => {
+                                        setValue(newValue)
+                                    }}
+                                    renderInput={(params) => <TextField {...params} /> }
+                                     />
+                                </LocalizationProvider>
+                            </Box>
+                            <Box>
+                                感想：<br />
+                                <TextField
+                                    multiline
+                                    fullWidth
+                                    rows={8} />
+                            </Box>
+                        </CardContent>
+                        <CardActions>
+                            <Grid container>
+                                <Grid item sm={6}>
+                                    <Button component={Link} to='/'
+                                        color="secondary" variant="contained">一覧に戻る</Button>
+                                </Grid>
+                                <Grid item sm={6}>
+                                    <Button color="info" variant="contained">保存する</Button>
+                                </Grid>
                             </Grid>
-                            <Grid item sm={6}>
-                                <Button color="info" variant="contained">保存する</Button>
-                            </Grid>
-                        </Grid>
-                    </CardActions>
+                        </CardActions>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Card>
+            </Card>
 
-      </Container>
+        </Container>
     </>)
 }
 
